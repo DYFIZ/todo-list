a7682b9266444c67702c9396b65471dde8efda59
mytsak ivan <mytsakivan@gmail.com>
2020-11-24 00:07:11 +0200

    main app component

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,67 @@
 import React, { Component } from "react";
-
+import ReactDOM from "react-dom";
+import TodoForm from "./todoForm";
+import TodoHeader from "./todoHeader";
+import TodoList from "./todoList";
 import '../styles/App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>My React App!</h1>
-            </div>
-        );
-    }
+/*
+Todo app structure
+
+TodoApp
+	- TodoHeader
+	- TodoList
+    - TodoListItem #1
+		- TodoListItem #2
+		  ...
+		- TodoListItem #N
+	- TodoForm
+*/
+var todoItems = [];
+todoItems.push({index: 1, value: "task 1", done: false});
+todoItems.push({index: 2, value: "task 2", done: true});
+todoItems.push({index: 3, value: "task 3", done: true});
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+     //Привязка методов из прототипа к конкретному экземпляру
+    this.addItem = this.addItem.bind(this);
+    this.removeItem = this.removeItem.bind(this);
+    this.markTodoDone = this.markTodoDone.bind(this);
+    this.state = {todoItems: todoItems};
+  }
+  addItem(todoItem) {
+    todoItems.unshift({
+      index: todoItems.length+1, 
+      value: todoItem.newItemValue, 
+      done: false
+    });
+  
+    this.setState({todoItems: todoItems});
+  }
+  removeItem (itemIndex) {
+    todoItems.splice(itemIndex, 1);
+    this.setState({todoItems: todoItems});
+  }
+  markTodoDone(itemIndex) {
+    var todo = todoItems[itemIndex];
+    todoItems.splice(itemIndex, 1);
+    todo.done = !todo.done;
+    todo.done ? todoItems.push(todo) : todoItems.unshift(todo);
+    this.setState({todoItems: todoItems});  
+  }
+  render() {
+    return (
+      <div id="main">
+        <TodoForm addItem={this.addItem}/>
+        <TodoHeader />
+        <TodoList items={this.props.initItems} removeItem={this.removeItem} markTodoDone={this.markTodoDone}/>
+      </div>
+    );
+  }
 }
 
-export default App;
+
+ReactDOM.render(<App initItems={todoItems}/>, document.getElementById("root"));
